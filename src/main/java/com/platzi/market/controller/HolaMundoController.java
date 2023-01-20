1c15678c19e01cc8029318ff78ad4e5453487e5b
Roberto Lopez <ing.robertolc@gmail.com>
2023-01-20 17:34:30 -0600

    first commit

---
+++ src/main/java/com/platzi/market/controller/HolaMundoController.java
@@ -0,0 +1,15 @@
+package com.platzi.market.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/saludar")
+public class HolaMundoController {
+
+    @GetMapping
+    public String saludar(){
+        return "Nunca pares de aprender!";
+    }
+}
