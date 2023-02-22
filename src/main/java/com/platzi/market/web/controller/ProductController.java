63c4a4c38311856413ed3d9d3097ba39db577d5d
Roberto Lopez <ing.robertolc@gmail.com>
2023-02-21 19:34:48 -0600

    implementamos la documentación con swagger

--- src/main/java/com/platzi/market/web/controller/ProductController.java
+++ src/main/java/com/platzi/market/web/controller/ProductController.java
@@ -2,6 +2,10 @@ package com.platzi.market.web.controller;
 
 import com.platzi.market.domain.Product;
 import com.platzi.market.domain.service.ProductService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,14 +22,22 @@ public class ProductController {
     private ProductService productService;
 
     @GetMapping()
+    @ApiOperation("Get all supermarket products")
+    @ApiResponse(code = 200, message = "OK")
     //public List<Product> getAll() {
     public ResponseEntity<List<Product>> getAll() {
         return new ResponseEntity<>(productService.getAll(), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
+    @ApiOperation("Search a product with an ID") //Swagger Documentation
+    @ApiResponses({ //Swagger Documentation
+            @ApiResponse(code = 200, message = "OK"),
+            @ApiResponse(code = 404, message = "Product not found"),
+    })
     //public Optional<Product> getProduct(@PathVariable("id") int productId) {
-    public ResponseEntity<Product> getProduct(@PathVariable("id") int productId) {
+    public ResponseEntity<Product> getProduct(@ApiParam(value = "The id of the product", required = true, example = "7") //Swagger Documentation
+                                                  @PathVariable("id") int productId) {
         return productService.getProduct(productId).
                 map(product -> new ResponseEntity<>(product, HttpStatus.OK))
                 .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
