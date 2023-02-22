ab63aa68be2191cd7f701abe7d5ed86025470a12
Roberto Lopez <ing.robertolc@gmail.com>
2023-02-21 18:26:50 -0600

    se realiza la implementacion del endpoint de compra

---
+++ src/main/java/com/platzi/market/web/controller/PurchaseController.java
@@ -0,0 +1,36 @@
+package com.platzi.market.web.controller;
+
+import com.platzi.market.domain.Purchase;
+import com.platzi.market.domain.service.PurchaseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/purchase")
+public class PurchaseController {
+
+    @Autowired
+    PurchaseService purchaseService;
+
+    @GetMapping
+    public ResponseEntity<List<Purchase>> getAll() {
+        return new ResponseEntity<>(purchaseService.getAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<List<Purchase>> getByClient(@PathVariable("id") String clientId) {
+        return purchaseService.getByClient(clientId)
+                .map(purchase -> new ResponseEntity<>(purchase, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping
+    public ResponseEntity<Purchase> save(@RequestBody Purchase purchase) {
+        return new ResponseEntity<>(purchaseService.save(purchase), HttpStatus.CREATED);
+    }
+}
